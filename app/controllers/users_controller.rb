2578a70acac26370a05f3da9e83d7fa0e8b8f10f
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2018-12-26 14:00:35 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      flash[:success] = "Welcome to My Diary!"
       session[:user_id] = user.id
       redirect_to mypage_path
     else
